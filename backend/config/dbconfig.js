634602e570ee870a66e52b8340e9873f10172e96
Sudipta077 <paulsudipta929@gmail.com>
2024-12-17 19:00:44 +0530

    issue fix

--- backend/config/dbconfig.js
+++ backend/config/dbconfig.js
@@ -5,8 +5,6 @@ require('dotenv').config(); // To load environment variables from .env file
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
             
         });
         console.log('MongoDB connected successfully');
