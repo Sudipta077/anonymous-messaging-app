3fadd85217b3eb868769a2c62274378efb1fc498
Sudipta077 <paulsudipta929@gmail.com>
2024-12-17 20:12:41 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -6,22 +6,25 @@ const authRoute = require('./routes/authRoute');
 const userRoute = require('./routes/userRoute');
 const connectDB = require('./config/dbconfig');
 
-
 connectDB();
 
-app.use(cors());
+
+app.use(cors({
+    origin: 'https://secret-messaging-plum.vercel.app', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    credentials: true 
+}));
+
 app.use(express.json());
 
-const PORT = process.env.PORT
 
-app.get('/',(req,res)=>{
-    res.send("Hello")
-})
+const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+    res.send("Hello");
+});
 
 app.use('/auth', authRoute);
 app.use('/user', userRoute);
-
-
 
 module.exports = app;
