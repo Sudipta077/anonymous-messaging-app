da4f4efcc2e356c963e2f08e22cbb158406bae1c
Sudipta077 <paulsudipta929@gmail.com>
2024-12-19 10:15:42 +0530

    message

--- frontend/src/pages/Message.jsx
+++ frontend/src/pages/Message.jsx
@@ -108,46 +108,46 @@ function Message(props) {
 
 
     return (
-        <div className='bg-primary'>
-        <h1 className='bg-secondary sm:w-fit w-full m-auto text-4xl font-myfont text-center p-5 hover:cursor-pointer text-primary hover:scale-105 transition' onClick={()=>navigate('/register')}>Create Your Account</h1>
-        <div className='bg-primary min-h-screen grid place-content-center px-2'>
-            <form onSubmit={handleSubmit} className='flex flex-col sm:flex-row gap-2 px-2'>
-                <input
-                    required
-                    type="text"
-                    className='sm:h-full h-24 w-96 focus:outline-none text-secondary bg-primary rounded px-2 py-1 border border-secondary font-myfont2'
-                    value={text}
-                    onChange={(e) => setText(e.target.value)}
-                    placeholder='Write message'
-                    maxLength="100"
-                />
+        <div className='bg-primary min-h-screen'>
+            <h1 className='bg-secondary sm:w-fit w-[80%] rounded-b-lg m-auto text-2xl font-myfont text-center p-5 hover:cursor-pointer text-primary hover:scale-105 transition' onClick={() => navigate('/register')}>Create Your Account</h1>
+            <div className='bg-primary mt-10 sm:w-96 w-[80%] m-auto'>
+                <form onSubmit={handleSubmit} className='flex flex-col sm:flex-row gap-2 w-full  sm:h-12'>
+                    <input
+                        required
+                        type="text"
+                        className='sm:h-full h-32 w-full sm:w-96  focus:outline-none text-secondary bg-primary rounded px-2 py-1 border border-secondary font-myfont2 m-auto'
+                        value={text}
+                        onChange={(e) => setText(e.target.value)}
+                        placeholder='Write message'
+                        maxLength="100"
+                    />
 
-                <button type='submit' className='bg-secondary text-primary w-full sm:w-24 sm:h-full font-myfont2'>Send</button>
-            </form>
-
-
-            <p className='my-5 font-myfont2 text-secondary'>Some suggestions for you. Click to use it.</p>
-            <div className='flex flex-col-reverse sm:flex-row justify-between items-center gap-x-5 '>
-
-                {loading === true ?
-
-                    <span className="mt-5 sm:mt-0 hover:cursor-pointer font-myfont2 text-primary p-2 rounded bg-secondary text-2xl ">Loading...</span> :
-                    <p className='mt-5 sm:mt-0 hover:cursor-pointer font-myfont2 text-primary p-2 rounded bg-secondary text-2xl w-96' onClick={handleCopy}>{suggestion}</p>
-
-                }
-
-                <h1
-                    ref={dice}
-                    className='text-4xl cursor-pointer w-fit m-auto'
-                    onClick={rollDice}
-                >
-                    🎲
-                </h1>
+                    <button type='submit' className='bg-secondary text-primary w-full sm:w-24 sm:h-full font-myfont2'>Send</button>
+                </form>
 
 
+                <p className='my-5 font-myfont2 text-secondary m-auto'>Some suggestions for you. Click to use it.</p>
+                <div className='flex flex-col-reverse sm:flex-row justify-between items-center gap-x-5 '>
+
+                    {loading === true ?
+
+                        <span className="mt-5 sm:mt-0 hover:cursor-pointer font-myfont2 text-primary p-2 rounded bg-secondary text-2xl w-11/12 h-fit ">Loading...</span> :
+                        <p className='mt-5 sm:mt-0 hover:cursor-pointer font-myfont2 text-primary p-2 rounded bg-secondary text-2xl w-full' onClick={handleCopy}>{suggestion}</p>
+
+                    }
+
+                    <h1
+                        ref={dice}
+                        className='text-4xl cursor-pointer w-fit m-auto'
+                        onClick={rollDice}
+                    >
+                        🎲
+                    </h1>
+
+
+                </div>
+                <ToastContainer />
             </div>
-            <ToastContainer />
-        </div>
         </div>
     );
 }
